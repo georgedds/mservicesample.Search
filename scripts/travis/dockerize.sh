5431189872a3ed234c2949d0385f7b1102790d5d
George Dedes <gdedes@vithoulkascompass.com>
2020-03-10 17:07:41 +0200

    test script
    test script

--- scripts/travis/dockerize.sh
+++ scripts/travis/dockerize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TAG='dev'
+TAG=''
 BUILD_NO=
 
 case "$GIT_BRANCH" in
@@ -11,7 +11,7 @@ esac
 
 REPOSITORY= mservicesample/mservicesample.search.api
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -t $REPOSITORY:$TAG -t $REPOSITORY:$BUILD_NO .
-docker push $REPOSITORY:$TAG
-docker push $REPOSITORY:$BUILD_NO
+echo $REPOSITORY:$TAG
+echo $REPOSITORY:$BUILD_NO
+echo $GIT_BRANCH
+
